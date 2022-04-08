fde2d8a3376c158fa44e874ea909d5be7b21dff7
Ian Robert O'Brien <13039736+ianrobrien@users.noreply.github.com>
2022-04-08 10:53:07 +0200

    Add new alias and repo directory format

---
+++ shell/fish/functions/cds.fish
@@ -0,0 +1,3 @@
+function cds --wraps=/Users/ianrobertobrien/go/pkg/mod/github.com/coopnorge/storm-121 --wraps='cd /Users/ianrobertobrien/go/pkg/mod/github.com/coopnorge/storm-121' --description 'alias cds=cd /Users/ianrobertobrien/go/pkg/mod/github.com/coopnorge/storm-121'
+  cd /Users/ianrobertobrien/go/pkg/mod/github.com/coopnorge/storm-121 $argv; 
+end
