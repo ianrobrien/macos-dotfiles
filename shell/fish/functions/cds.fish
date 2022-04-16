6c724d3cb3f1482bdae78f2bc3fe538f0778c069
Ian Robert O'Brien <13039736+ianrobrien@users.noreply.github.com>
2022-04-16 06:54:52 +0200

    Remove unusued function

--- shell/fish/functions/cds.fish
+++
@@ -1,3 +0,0 @@
-function cds --wraps=/Users/ianrobertobrien/go/pkg/mod/github.com/coopnorge/storm-121 --wraps='cd /Users/ianrobertobrien/go/pkg/mod/github.com/coopnorge/storm-121' --description 'alias cds=cd /Users/ianrobertobrien/go/pkg/mod/github.com/coopnorge/storm-121'
-  cd /Users/ianrobertobrien/go/pkg/mod/github.com/coopnorge/storm-121 $argv; 
-end
