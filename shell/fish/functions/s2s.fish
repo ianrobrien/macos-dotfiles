a77c458261d8dc720d0c28a15633f1a59e7fd48f
Ian Robert O'Brien <13039736+ianrobrien@users.noreply.github.com>
2022-04-18 11:18:50 +0200

    Rename sha256sum function to s2s

---
+++ shell/fish/functions/s2s.fish
@@ -0,0 +1,3 @@
+function s2s --wraps=sha256sum --description 'alias s2s=sha256sum'
+  sha256sum $argv;
+end
--- shell/fish/functions/ss.fish
+++
@@ -1,3 +0,0 @@
-function ss --wraps=sha256sum --description 'alias ss=sha256sum'
-  sha256sum $argv; 
-end
